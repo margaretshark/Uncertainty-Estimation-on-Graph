0d33e23479fbfb766174d2d6baca3555b0f28d31
Anastasia Tabisheva <tabisheva1999@mail.ru>
2020-08-17 23:21:32 +0000

    Added graph.py

---
+++ graph.py
@@ -0,0 +1,56 @@
+class Net(nn.Module):
+
+    def __init__(self, graph_type: str="GCN", dataset_name: str="cora", seed: int=42):
+        set_seed(seed)
+        if dataset_name == "cora":
+            self.graph, self.features, self.labels, self.train_mask, self.test_mask = load_cora_data()
+            self.num_classes = 7
+
+        self.features_dimension = len(self.features[0])
+
+        if graph_type == "GCN":
+            super(Net, self).__init__()
+            self.layer1 = GCNLayer(self.features_dimension, 16)
+            self.layer2 = GCNLayer(16, self.num_classes)
+
+    def forward(self):
+        x = F.relu(self.layer1(self.graph, self.features))
+        x = self.layer2(self.graph, x)
+        return x
+
+    def train_loop(self, num_epoch: int=50, lr: float=1e-2):
+        dur = []
+        optimizer = th.optim.Adam(self.parameters(), lr=1e-2)
+        for epoch in range(num_epoch):
+            if epoch >=3:
+                t0 = time.time()
+            self.train()
+            logits = self()
+            logp = F.log_softmax(logits, 1)
+            loss = F.nll_loss(logp[self.train_mask], labels[self.train_mask])
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            if epoch >=3:
+                dur.append(time.time() - t0)
+                
+            acc = calculate_accuracy(*self.evaluate())
+            
+            print("Epoch {:05d} | loss {:.4f} | Test Acc {:.4f} | Time(s) {:.4f}".format(
+            epoch, loss.item(), acc, np.mean(dur)))
+
+    def evaluate(self):
+        self.eval()
+        with th.no_grad():
+            logits = self()
+            logits = logits[self.test_mask]
+            labels = self.labels[self.test_mask]
+            return logits, labels
+
+
+def calculate_accuracy(logits, labels):
+    _, indices = th.max(logits, dim=1)
+    correct = th.sum(indices == labels)
+    return correct.item() * 1.0 / len(labels)
